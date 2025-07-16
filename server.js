712ece83c8cb1a1ce97a5460fd4ffa4781d04390
ganapathi9191 <ganapathivaraprasad123@gmail.com>
2025-07-16 11:55:52 +0530

    f00

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
     console.log(`🚀 server running on port ${PORT}`)
